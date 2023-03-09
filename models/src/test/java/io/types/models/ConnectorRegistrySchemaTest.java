72be3435e0b9e2c00cb2867c217ba7d6d102f410
Ben Church <ben@airbyte.io>
2023-03-08 18:12:28 -0800

    Ensure its building

--- models/src/test/java/io/types/models/ConnectorRegistrySchemaTest.java
+++ models/src/test/java/io/types/models/ConnectorRegistrySchemaTest.java
@@ -2,11 +2,12 @@
  * Copyright (c) 2022 Airbyte, Inc., all rights reserved.
  */
 
-package io.airbyte-types.models;
+package io.types.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import io.airbyte.types.models.ConnectorRegistry;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -14,6 +15,12 @@ import org.junit.jupiter.api.Test;
 
 class ConnectorRegistrySchemaTest {
 
+  @Test
+  void testFile() throws IOException {
+    // TODO actually write a test
+    assertTrue(true);
+  }
+//
 //  @Test
 //  void testFile() throws IOException {
 //    final String schema = Files.readString(ConnectorRegistry.PROTOCOL.getFile().toPath(), StandardCharsets.UTF_8);
